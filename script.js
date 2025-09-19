2ae3775ccf3937e9fc22f98d91f2c5164df67a42
eafranklin3991 <emifrank@uga.edu>
2025-09-19 11:43:03 -0400

    Load pitch video on demand, Remove iframes because you should not use those for videos

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ function toggleMenu() {
     const menu = document.getElementById("menu");
     menu.style.display = menu.style.display === "block" ? "none" : "block";
 }
+
+const pitchVideo = document.querySelector("#pitch-video");
+pitchVideo.setAttribute('preload', 'auto');
+pitchVideo.load();
